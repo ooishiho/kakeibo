fb4e15ef0b040895cb6067ab38b109c460e0f09d
ooishiho <yuidacheng697@gmail.com>
2023-10-28 13:27:00 +0000

    [add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get 'top' => 'homes#top'
+get 'lists/new'
+get 'lists/index'
+get 'lists/show'
+get 'lists/edit'
+
 
 end
