510124c17aa3ad17926b4a7ae0ff182219a8cc73
ooishiho <yuidacheng697@gmail.com>
2023-11-01 13:22:48 +0000

    [add]home view,index link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get 'top' => 'homes#top'
+root 'home#top'
+
+get 'top'
 get 'lists/new'
 post 'lists' => 'lists#create'
 get 'lists/index' => 'lists#index'
