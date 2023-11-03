dc591079abb08a0b5886df78560cf43023f87a47
ooishiho <yuidacheng697@gmail.com>
2023-11-03 13:40:58 +0000

    [fix]route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root 'home#top'
+root to: 'homes#top'
 
-get 'top'
 get 'lists/new'
 post 'lists' => 'lists#create'
 get 'lists/index' => 'lists#index'
-get 'lists/:id' => 'lists#show', as: 'list'
+get 'lists/:id' => 'lists#show', as:'list'
 get 'lists/edit'
 
 
