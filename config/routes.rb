a514ca55e8029a8f4183b712169f2f9d6fdb4246
ooishiho <yuidacheng697@gmail.com>
2023-11-05 13:17:37 +0000

    [fix]route, edit_view ,controlle

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: 'homes#top'
 
-get 'lists/new'
-post 'lists' => 'lists#create'
-get 'lists/index' => 'lists#index'
-get 'lists/:id' => 'lists#show', as:'list'
-get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-patch 'lists/:id' => 'lists#update', as: 'update_list'
-
+resources :lists
 end
