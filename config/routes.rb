be6cfac10f4055612ac08e0bc1e8c267bdcc76b8
ooishiho <yuidacheng697@gmail.com>
2023-11-16 13:00:52 +0000

    [add]tips

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: 'homes#top'
 
+  collection do
+    get 'tips'
+  end
+
 resources :lists
 end
