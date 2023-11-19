0ebda8e9bcffb782963b49cb3fdb85b5b16e0b33
ooishiho <yuidacheng697@gmail.com>
2023-11-19 13:44:22 +0000

    [add]category

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/edit'
+  get 'categories/update'
+  get 'categories/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: 'homes#top'
 
