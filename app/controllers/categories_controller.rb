31c6d301ee68d1461cc51f1e4c0a05c08f27ec59
ooishiho <yuidacheng697@gmail.com>
2023-11-20 13:15:14 +0000

    [add]migrate,controll

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,31 @@
 class CategoriesController < ApplicationController
   def index
-  end
-
-  def edit
-  end
-
-  def update
+    @category = Category.new
+    @categories = Category.all
   end
 
   def create
+    @category = Category.new(category_params)
+    @category.save
+    redirect_to categories_path
   end
+
+
+  def edit
+    @category = Category.new
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+    redirect_to categories_path
+  end
+
+private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_28_131740) do
+ActiveRecord::Schema.define(version: 2023_11_19_134021) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.integer "month"
